733cc08605fdd7751700a8c5cc934e975b956903
Eliza Knapp <elizaknapp@dhcp-10-250-41-54.harvard.edu>
2024-12-07 23:02:53 -0500

    code

--- generate_synthetic_data.py
+++ generate_synthetic_data.py
@@ -1,28 +1,50 @@
-class GenerateSyntheticData:
-    def __init__(self):
-        pass
-
-
-"""
 import openai
-import os
+import json
+import pandas as pd
 
-OPENAI_API_KEY = ""
-openai.api_key = OPENAI_API_KEY
+# Constants that we can alter later for LLM model
+LLM_MODEL = "gpt-3.5-turbo-1106"
 
-#code for a general request to gpt engine (change params as necessary)
-model_engine = "gpt-4"
-temperature = 0.7
-max_tokens = 400
-n = 1
+class GenerateSyntheticData:
+    def __init__(self, generation_instructions):
+        # Save an instance of generations of synthetic data with the system context
+        # NOTE: it must mention the json inputs and outputs to succeed
+        self.generation_instructions = generation_instructions
 
-def response(prompt):
-    return openai.ChatCompletion.create(
-        model="gpt-4",
-        messages=[
-            {"role": "system", "content": "You generate sentences on the basis of content I present you. Make the sentences as different from each other as possible while still relating to the topic. Use plain, everyday language. Do not use metaphors or similes."},
-            {"role": "user", "content": prompt}
+    def to_message(self, data_sample):
+        """
+        Takes all the information and converts it straight into llm input
+        """
+        # TODO: verify if this actually works as an llm input (the json file)
+        prompt = data_sample.to_json()
+        messages = [
+            {"role": "system", "content": self.generation_instructions},
+            {"role": "user", "content": prompt},
         ]
-    )
-
-"""
+        return messages
+    
+    def validate_json(self, text):
+        try:
+            return json.loads(text)
+        except:
+            raise Exception("Json didn't generate correctly")
+    
+    def predict(self, api_key, data_sample):
+        """
+        api_key: openai api key to be passed in from main runner file
+        data_sample: pandas df that we should base the generation data on
+        """
+        # Setup api key
+        openai.api_key = api_key
+        # Prepare response parameters
+        response_parameters = {
+            "model": LLM_MODEL,
+            "messages": self.to_message(data_sample),
+            "response_format": {"type": "json_object"}
+        }
+        # Get the response
+        response = openai.ChatCompletion.create(**response_parameters)
+        generated_json = response["choices"][0]["message"]["content"]
+        # Make sure the format is correct and raise error if not
+        final_json = self.validate_json(generated_json)
+        return pd.read_json(final_json)
